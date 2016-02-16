90e5c175b52a06db70779826ab89dce3a1a45a32
zhong <zhong@sumologic.com>
2016-02-16 11:09:56 -0800

    Add file

---
+++ MovingAvgWithDifferentWindowSize.mk
@@ -0,0 +1,142 @@
+|fileName | data size | outlier num| max | min | avg | std
+|---|---|---|---|---|---|---
+|cpu_utilization_asg_misconfiguration.csv | 18050 | 2 | 100.0| 11.529000000000002| 38.28275604986151| 15.639293830231823
+
+|Algorithm| MovingAvgModelParam(30,20.0,1)| MovingAvgModelParam(20,20.0,1)| MovingAvgModelParam(10,20.0,1)| MovingAvgModelParam(5,20.0,1)| 
+|--- |--- |--- |--- |---
+|Outlier Found| 0| 0| 8| 30| 
+|True Outliers| 0| 0| 0| 0| 
+|Precision| 100.0| 100.0| 0.0| 0.0| 
+|Recall| 0.0| 0.0| 0.0| 0.0| 
+|F1Scire| 0.0| 0.0| 0.0| 0.0| 
+|False Positive Rate| 0.0| 0.0| 4.432624113475177E-4| 0.0016622340425531915| 
+
+|fileName | data size | outlier num| max | min | avg | std
+|---|---|---|---|---|---|---
+|ec2_cpu_utilization_24ae8d.csv | 4032 | 2 | 2.344| 0.066| 0.12630307539682542| 0.0948128470814252
+
+|Algorithm| MovingAvgModelParam(30,20.0,1)| MovingAvgModelParam(20,20.0,1)| MovingAvgModelParam(10,20.0,1)| MovingAvgModelParam(5,20.0,1)| 
+|--- |--- |--- |--- |---
+|Outlier Found| 15| 16| 35| 183| 
+|True Outliers| 1| 2| 2| 2| 
+|Precision| 0.06666666666666667| 0.125| 0.05714285714285714| 0.01092896174863388| 
+|Recall| 0.5| 1.0| 1.0| 1.0| 
+|F1Scire| 0.11764705882352941| 0.2222222222222222| 0.1081081081081081| 0.021621621621621623| 
+|False Positive Rate| 0.0034739454094292804| 0.0034739454094292804| 0.008188585607940446| 0.044913151364764266| 
+
+|fileName | data size | outlier num| max | min | avg | std
+|---|---|---|---|---|---|---
+|ec2_cpu_utilization_53ea38.csv | 4032 | 2 | 2.656| 1.604| 1.8295550595238095| 0.1014579379208487
+
+|Algorithm| MovingAvgModelParam(30,20.0,1)| MovingAvgModelParam(20,20.0,1)| MovingAvgModelParam(10,20.0,1)| MovingAvgModelParam(5,20.0,1)| 
+|--- |--- |--- |--- |---
+|Outlier Found| 0| 0| 0| 3| 
+|True Outliers| 0| 0| 0| 0| 
+|Precision| 100.0| 100.0| 100.0| 0.0| 
+|Recall| 0.0| 0.0| 0.0| 0.0| 
+|F1Scire| 0.0| 0.0| 0.0| 0.0| 
+|False Positive Rate| 0.0| 0.0| 0.0| 7.444168734491315E-4| 
+
+|fileName | data size | outlier num| max | min | avg | std
+|---|---|---|---|---|---|---
+|ec2_cpu_utilization_5f5533.csv | 4032 | 2 | 68.092| 34.766| 43.11037160218254| 4.303564641024773
+
+|Algorithm| MovingAvgModelParam(30,20.0,1)| MovingAvgModelParam(20,20.0,1)| MovingAvgModelParam(10,20.0,1)| MovingAvgModelParam(5,20.0,1)| 
+|--- |--- |--- |--- |---
+|Outlier Found| 0| 0| 0| 1| 
+|True Outliers| 0| 0| 0| 0| 
+|Precision| 100.0| 100.0| 100.0| 0.0| 
+|Recall| 0.0| 0.0| 0.0| 0.0| 
+|F1Scire| 0.0| 0.0| 0.0| 0.0| 
+|False Positive Rate| 0.0| 0.0| 0.0| 2.4813895781637717E-4| 
+
+|fileName | data size | outlier num| max | min | avg | std
+|---|---|---|---|---|---|---
+|ec2_cpu_utilization_77c1ca.csv | 4032 | 1 | 99.898| 0.064| 10.518176091269847| 26.92863403853015
+
+|Algorithm| MovingAvgModelParam(30,20.0,1)| MovingAvgModelParam(20,20.0,1)| MovingAvgModelParam(10,20.0,1)| MovingAvgModelParam(5,20.0,1)| 
+|--- |--- |--- |--- |---
+|Outlier Found| 38| 47| 85| 165| 
+|True Outliers| 0| 0| 0| 0| 
+|Precision| 0.0| 0.0| 0.0| 0.0| 
+|Recall| 0.0| 0.0| 0.0| 0.0| 
+|F1Scire| 0.0| 0.0| 0.0| 0.0| 
+|False Positive Rate| 0.009426941205656165| 0.011659637806995783| 0.021086579012651947| 0.04093277102455966| 
+
+|fileName | data size | outlier num| max | min | avg | std
+|---|---|---|---|---|---|---
+|ec2_cpu_utilization_c6585a.csv | 4032 | 0 | 1.6019999999999999| 0.062| 0.08694841269841269| 0.08521157474669312
+
+|Algorithm| MovingAvgModelParam(30,20.0,1)| MovingAvgModelParam(20,20.0,1)| MovingAvgModelParam(10,20.0,1)| MovingAvgModelParam(5,20.0,1)| 
+|--- |--- |--- |--- |---
+|Outlier Found| 14| 14| 14| 164| 
+|True Outliers| 0| 0| 0| 0| 
+|Precision| 0.0| 0.0| 0.0| 0.0| 
+|Recall| 100.0| 100.0| 100.0| 100.0| 
+|F1Scire| 0.0| 0.0| 0.0| 0.0| 
+|False Positive Rate| 0.003472222222222222| 0.003472222222222222| 0.003472222222222222| 0.040674603174603176| 
+
+|fileName | data size | outlier num| max | min | avg | std
+|---|---|---|---|---|---|---
+|ec2_cpu_utilization_fe7f93.csv | 4032 | 3 | 99.66799999999999| 1.8| 5.77896378968254| 11.811688561419643
+
+|Algorithm| MovingAvgModelParam(30,20.0,1)| MovingAvgModelParam(20,20.0,1)| MovingAvgModelParam(10,20.0,1)| MovingAvgModelParam(5,20.0,1)| 
+|--- |--- |--- |--- |---
+|Outlier Found| 25| 33| 45| 68| 
+|True Outliers| 0| 0| 0| 0| 
+|Precision| 0.0| 0.0| 0.0| 0.0| 
+|Recall| 0.0| 0.0| 0.0| 0.0| 
+|F1Scire| 0.0| 0.0| 0.0| 0.0| 
+|False Positive Rate| 0.0062050136510300325| 0.008190618019359643| 0.011169024571854059| 0.016877637130801686| 
+
+|fileName | data size | outlier num| max | min | avg | std
+|---|---|---|---|---|---|---
+|ec2_disk_write_bytes_c0d644.csv | 4032 | 3 | 8.63964E8| 0.0| 1.7331273319295567E7| 7.969664473491499E7
+
+|Algorithm| MovingAvgModelParam(30,20.0,1)| MovingAvgModelParam(20,20.0,1)| MovingAvgModelParam(10,20.0,1)| MovingAvgModelParam(5,20.0,1)| 
+|--- |--- |--- |--- |---
+|Outlier Found| 73| 88| 146| 187| 
+|True Outliers| 0| 0| 0| 0| 
+|Precision| 0.0| 0.0| 0.0| 0.0| 
+|Recall| 0.0| 0.0| 0.0| 0.0| 
+|F1Scire| 0.0| 0.0| 0.0| 0.0| 
+|False Positive Rate| 0.018118639861007695| 0.021841648051625712| 0.03623727972201539| 0.046413502109704644| 
+
+|fileName | data size | outlier num| max | min | avg | std
+|---|---|---|---|---|---|---
+|grok_asg_anomaly.csv | 4621 | 3 | 45.6229| 0.0| 27.68472343864973| 13.144794768553139
+
+|Algorithm| MovingAvgModelParam(30,20.0,1)| MovingAvgModelParam(20,20.0,1)| MovingAvgModelParam(10,20.0,1)| MovingAvgModelParam(5,20.0,1)| 
+|--- |--- |--- |--- |---
+|Outlier Found| 14| 32| 69| 215| 
+|True Outliers| 0| 0| 0| 0| 
+|Precision| 0.0| 0.0| 0.0| 0.0| 
+|Recall| 0.0| 0.0| 0.0| 0.0| 
+|F1Scire| 0.0| 0.0| 0.0| 0.0| 
+|False Positive Rate| 0.0030316154179298397| 0.006929406669553919| 0.01494153313122564| 0.046556951061065395| 
+
+|fileName | data size | outlier num| max | min | avg | std
+|---|---|---|---|---|---|---
+|iio_us-east-1_i-a2eb1cd9_NetworkIn.csv | 1243 | 2 | 6.1519397E7| 789781.0| 4615221.908447306| 4534241.665066064
+
+|Algorithm| MovingAvgModelParam(30,20.0,1)| MovingAvgModelParam(20,20.0,1)| MovingAvgModelParam(10,20.0,1)| MovingAvgModelParam(5,20.0,1)| 
+|--- |--- |--- |--- |---
+|Outlier Found| 0| 1| 1| 2| 
+|True Outliers| 0| 0| 0| 0| 
+|Precision| 100.0| 0.0| 0.0| 0.0| 
+|Recall| 0.0| 0.0| 0.0| 0.0| 
+|F1Scire| 0.0| 0.0| 0.0| 0.0| 
+|False Positive Rate| 0.0| 8.058017727639E-4| 8.058017727639E-4| 0.0016116035455278| 
+
+|fileName | data size | outlier num| max | min | avg | std
+|---|---|---|---|---|---|---
+|rds_cpu_utilization_e47b3b.csv | 4032 | 2 | 76.23| 12.628| 18.934867559523813| 5.605191488864253
+
+|Algorithm| MovingAvgModelParam(30,20.0,1)| MovingAvgModelParam(20,20.0,1)| MovingAvgModelParam(10,20.0,1)| MovingAvgModelParam(5,20.0,1)| 
+|--- |--- |--- |--- |---
+|Outlier Found| 1| 1| 1| 6| 
+|True Outliers| 1| 1| 1| 1| 
+|Precision| 1.0| 1.0| 1.0| 0.16666666666666666| 
+|Recall| 0.5| 0.5| 0.5| 0.5| 
+|F1Scire| 0.6666666666666666| 0.6666666666666666| 0.6666666666666666| 0.25| 
+|False Positive Rate| 0.0| 0.0| 0.0| 0.0012406947890818859| 
